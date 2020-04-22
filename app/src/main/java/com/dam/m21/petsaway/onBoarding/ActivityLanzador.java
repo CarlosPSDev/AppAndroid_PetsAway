1e4ff5ef37c872fe3c56bbd707da2fad44f8e80b
carlos <tuotramedicina@gmail.com>
2020-04-23 00:09:52 +0200

    .

---
+++ app/src/main/java/com/dam/m21/petsaway/onBoarding/ActivityLanzador.java
@@ -0,0 +1,16 @@
+package com.dam.m21.petsaway.onBoarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dam.m21.petsaway.R;
+
+public class ActivityLanzador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lanzador);
+    }
+}
