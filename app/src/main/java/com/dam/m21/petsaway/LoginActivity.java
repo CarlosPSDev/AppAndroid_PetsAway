d19198d581051f87f534de851b0bc5045d4c76e9
danielSBPC <daniel.sanchezbo@gmail.com>
2020-04-24 19:35:55 +0200

    SplashScreen y LoginActivity

--- app/src/main/java/com/dam/m21/petsaway/LoginActivity.java
+++ app/src/main/java/com/dam/m21/petsaway/LoginActivity.java
@@ -3,6 +3,7 @@ package com.dam.m21.petsaway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getSupportActionBar().hide();
+    }
+
+    public void contraseniaOlvidada(View view) {
+    }
+
+    public void accesoRegistro(View view) {
+    }
+
+    public void accesoAplicacion(View view) {
+    }
+
+    public void loginGmail(View view) {
+    }
+
+    public void loginFacebook(View view) {
     }
 }
