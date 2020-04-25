a7bfcd2d936f3287ead940518050aefcf7adda59
carlos <tuotramedicina@gmail.com>
2020-04-25 23:46:59 +0200

    .

--- app/src/main/java/com/dam/m21/petsaway/onBoarding/LanzadorOnBoard.java
+++ app/src/main/java/com/dam/m21/petsaway/onBoarding/LanzadorOnBoard.java
@@ -2,20 +2,18 @@ package com.dam.m21.petsaway.onBoarding;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
-import android.content.Intent;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
-import android.widget.Toast;
-import com.dam.m21.petsaway.MainActivity;
+
 import com.dam.m21.petsaway.R;
 
-public class ActivityLanzador extends AppCompatActivity {
+public class LanzadorOnBoard extends AppCompatActivity {
     ViewPager viewpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lanzador);
+        setContentView(R.layout.lanzador_onboard);
         /*Este bloque debería descomentarse al lanzar la app final. También podría ir tras validar el login
         para que compruebe si hay q llamar a este activity o directamente no*/
       /*  SharedPreferences sharedPrefs = getSharedPreferences("ArchivoVeces", MODE_PRIVATE);
