3151d3c25359c6cc8e43973dae5a0ccb581f0de0
carlos <tuotramedicina@gmail.com>
2020-04-25 19:33:14 +0200

    .

--- app/src/main/java/com/dam/m21/petsaway/onBoarding/ActivityLanzador.java
+++ app/src/main/java/com/dam/m21/petsaway/onBoarding/ActivityLanzador.java
@@ -15,8 +15,24 @@ public class ActivityLanzador extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lanzador);
 
+         /*Este bloque debería descomentarse al lanzar la app final. También podría ir tras validar el login
+        para que compruebe si hay q llamar a este activity o directamente no*/
+         
+      /*  SharedPreferences sharedPrefs = getSharedPreferences("ArchivoVeces", MODE_PRIVATE);
+        int numVeces = sharedPrefs.getInt("vecesEjecutado", 0);
+
+        if (numVeces > 0) {
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        } else {
+            numVeces++;
+            SharedPreferences.Editor editor = sharedPrefs.edit();
+            editor.putInt("vecesEjecutado", numVeces);
+            editor.commit();*/
+
         viewpager = findViewById(R.id.viewPager);
         OnBoardAdapter oba = new OnBoardAdapter(getSupportFragmentManager());
         viewpager.setAdapter(oba);
+    //}
     }
 }
