0bee61415b7b474c07b4ef33c4298c0d4e14e8fd
danielSBPC <daniel.sanchezbo@gmail.com>
2020-04-27 01:25:03 +0200

    Rediseño onBoarding. Mantenidas tus clases Carlos.

--- app/src/main/java/com/dam/m21/petsaway/onBoarding/OnBoard3Fragment.java
+++ app/src/main/java/com/dam/m21/petsaway/onBoarding/OnBoard3Fragment.java
@@ -32,6 +32,7 @@ public class OnBoard3Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_onboard3, container, false);
+        
         viewPager = getActivity().findViewById(R.id.viewPager);
         ivFin = view.findViewById(R.id.ivSl3Sig);
         ivAnterior = view.findViewById(R.id.ivSl3Ant);
