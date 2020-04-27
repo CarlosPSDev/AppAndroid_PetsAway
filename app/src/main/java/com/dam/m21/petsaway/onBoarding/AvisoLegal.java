ac88a258bd102f0a737779506bf0e4a08d54dced
danielSBPC <daniel.sanchezbo@gmail.com>
2020-04-27 23:35:42 +0200

    Añadida la clase RegistroActivity, y login con firebase.

--- app/src/main/java/com/dam/m21/petsaway/onBoarding/AvisoLegal.java
+++ app/src/main/java/com/dam/m21/petsaway/onBoarding/AvisoLegal.java
@@ -12,5 +12,6 @@ public class AvisoLegal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aviso_legal);
+        
     }
 }
