34d31611aad7b6042b78639fb5a6a6bf77de7e5a
zoria <zoriaska.1838@gmail.com>
2020-05-18 17:48:47 +0200

    InvitarAmiguwis

--- app/src/main/java/com/dam/m21/petsaway/ajustes/AjustesActivity.java
+++ app/src/main/java/com/dam/m21/petsaway/ajustes/AjustesActivity.java
@@ -1,25 +1,28 @@
 package com.dam.m21.petsaway.ajustes;
-
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.dam.m21.petsaway.login.LoginActivity;
 import com.dam.m21.petsaway.model.PojoUser;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -28,12 +31,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.dynamiclinks.DynamicLink;
+import com.google.firebase.dynamiclinks.FirebaseDynamicLinks;
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData;
+import com.google.firebase.dynamiclinks.ShortDynamicLink;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import com.dam.m21.petsaway.R;
 import com.google.firebase.storage.UploadTask;
 
@@ -46,10 +51,13 @@ public class AjustesActivity extends AppCompatActivity {
     DatabaseReference dbr;
     String tema,idUser;
     Button bt_tO,bt_tC;
+    AlertDialog.Builder bd1;
 
     static final int REQUEST_CODE_PASS =1;
     static final int REQUEST_CODE_EMAIL =2;
     static final String CODE_DATO ="DATO";
+
+    FirebaseAnalytics analytics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,6 +73,18 @@ public class AjustesActivity extends AppCompatActivity {
         bt_tC=findViewById(R.id.bt_tC);
         tema="";
         datosUser();
+        FirebaseDynamicLinks.getInstance().getDynamicLink(getIntent())
+                .addOnSuccessListener(this, new OnSuccessListener<PendingDynamicLinkData>() {
+                    @Override
+                    public void onSuccess(PendingDynamicLinkData pendingDynamicLinkData) {
+                        if (pendingDynamicLinkData != null) {
+                            analytics = FirebaseAnalytics.getInstance(AjustesActivity.this);
+                        }
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {}
+                });
     }
 
     public void datosUser() {
@@ -195,7 +215,21 @@ public class AjustesActivity extends AppCompatActivity {
     }
 
     public void borrarMiCuenta(View view) {
-        fu.delete();
+        bd1= new androidx.appcompat.app.AlertDialog.Builder(AjustesActivity.this);
+        bd1.setCancelable(false);
+        bd1.setMessage(R.string.msjDialogborrarCuenta);
+        bd1.setPositiveButton(R.string.msjBtDialogSI, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                fu.delete();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+        bd1.setNegativeButton(R.string.msjBtDialogNo, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        bd1.create().show();
     }
 
     public void cambiarCorreo(View view) {
@@ -212,7 +246,36 @@ public class AjustesActivity extends AppCompatActivity {
         startActivityForResult(intent, REQUEST_CODE_PASS);
 
     }
+    public static Uri buildDynamicLink() {
+        Uri baseUrl = Uri.parse("https://petsaway.page.link/4Yif");
+        String domain = "https://petsaway.page.link";
 
-	public void invitarAmigos(View view) {
+        DynamicLink link = FirebaseDynamicLinks.getInstance().createDynamicLink()
+                .setLink(baseUrl).setDomainUriPrefix(domain)
+                .setIosParameters(new DynamicLink.IosParameters.Builder("com.dam.m21.petsaway").build())
+                .setAndroidParameters(new DynamicLink.AndroidParameters.Builder("com.dam.m21.petsaway").build())
+                .setSocialMetaTagParameters(new DynamicLink.SocialMetaTagParameters.Builder().setTitle("Share this App").setDescription("blabla").build())
+                .setGoogleAnalyticsParameters(new DynamicLink.GoogleAnalyticsParameters.Builder().setSource("AndroidApp").build())
+                .buildDynamicLink();
+
+        return link.getUri();
+    }
+
+    public void invitarAmigos(View view) {
+        FirebaseDynamicLinks.getInstance().createDynamicLink()
+            .setLongLink(buildDynamicLink()).buildShortDynamicLink()
+            .addOnCompleteListener(this, new OnCompleteListener<ShortDynamicLink>() {
+                @Override
+                public void onComplete(@NonNull Task<ShortDynamicLink> task) {
+                    if (task.isSuccessful()) {
+                        Uri shortLink = task.getResult().getShortLink();
+                        Intent intent = new Intent(Intent.ACTION_SEND);
+                        String msg = "visit my awesome website: " + shortLink.toString();
+                        intent.putExtra(Intent.EXTRA_TEXT, msg);
+                        intent.setType("text/plain");
+                        startActivity(Intent.createChooser(intent, "Share Link"));
+                    }
+                }
+            });
 	}
 }
