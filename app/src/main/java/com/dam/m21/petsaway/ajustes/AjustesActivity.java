7a513834a6dbb4a28d57c2d5b91f7340ed3b9f95
zoria <zoriaska.1838@gmail.com>
2020-05-18 17:49:13 +0200

    InvitarAmiguwis

--- app/src/main/java/com/dam/m21/petsaway/ajustes/AjustesActivity.java
+++ app/src/main/java/com/dam/m21/petsaway/ajustes/AjustesActivity.java
@@ -11,7 +11,6 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
