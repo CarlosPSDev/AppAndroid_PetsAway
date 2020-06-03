cdaee7a73fe630dc098f76f3cc1e628583f925dd
carlos <tuotramedicina@gmail.com>
2020-06-03 13:49:46 +0200

    habilitación del onboarding

--- app/src/main/java/com/dam/m21/petsaway/on_boarding/LanzadorOnBoard.java
+++ app/src/main/java/com/dam/m21/petsaway/on_boarding/LanzadorOnBoard.java
@@ -16,24 +16,8 @@ public class LanzadorOnBoard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lanzador_onboard);
 
-         /*Este bloque debería descomentarse al lanzar la app final. También podría ir tras validar el login
-        para que compruebe si hay q llamar a este activity o directamente no*/
-
-       /* SharedPreferences sharedPrefs = getSharedPreferences("ArchivoVeces", MODE_PRIVATE);
-        int numVeces = sharedPrefs.getInt("vecesEjecutado", 0);
-
-        if (numVeces > 0) {
-            Intent i = new Intent(this, MainActivity.class);
-            startActivity(i);
-        } else {
-            numVeces++;
-            SharedPreferences.Editor editor = sharedPrefs.edit();
-            editor.putInt("vecesEjecutado", numVeces);
-            editor.commit();*/
-
         viewpager = findViewById(R.id.viewPager);
         OnBoardAdapter oba = new OnBoardAdapter(getSupportFragmentManager());
         viewpager.setAdapter(oba);
-    //}
     }
 }
